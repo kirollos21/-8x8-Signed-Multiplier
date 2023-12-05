cbc1d6bcbd98f699d371c9f20ad7b727ced575c9
Tawfik96 <112944729+Tawfik96@users.noreply.github.com>
2023-12-05 17:03:26 +0200

    Create shift_right_register

--- project_1/project_1.srcs/sources_1/new/shift_right_register.v
+++ project_1/project_1.srcs/sources_1/new/shift_right_register.v
@@ -20,7 +20,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module shift_right_register(
-
-    );
+module shift_right_register( input clk, load, en,[7:0]Num, output reg[7:0]Out );
+integer i;
+always @(posedge clk) begin 
+    if(load)begin
+        Out<=Num;
+    end
+    else begin
+        if(en)begin 
+            for(i=7; i>=0;i=i-1)begin
+                Out[i-1]<=Out[i];
+            end
+             Out[7]<=1'b0;
+         end
+    end
+end
 endmodule
